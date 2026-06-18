c5338b246af44675e5b5d637cef78ba72195e9b4
Mrostone <114079837+Mrostone@users.noreply.github.com>
2026-06-19 00:00:52 +0200

    feat: lexer return token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 package main.java;
 
 import main.java.lexing.Lexer;
+import main.java.lexing.LexerToken;
 
 class Main {
     public static void main(String[] args){
-        Lexer lexer = new Lexer();
-        lexer.lexing(args[0]);
+        Lexer lexer = new Lexer(args[0]);
+        LexerToken token = lexer.getToken();
+        while (token != null) {
+            System.out.println(token.getText());
+            token = lexer.getToken();
+        }
+
+        System.out.println("========= END ==========");
     }
 }
--- src/main/java/lexing/Lexer.java
+++ src/main/java/lexing/Lexer.java
@@ -1,35 +1,47 @@
 package main.java.lexing;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.PushbackReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Lexer {
     String currentToken = "";
+    PushbackReader pushbackReader;
 
-    public void lexing(String filename){
-        try (PushbackReader pushbackReader = new PushbackReader(new BufferedReader(new FileReader(filename)), 1);) {
-
-            int ValueChar;
-            while ((ValueChar = pushbackReader.read()) != -1) {
-                char c = (char) ValueChar;
-                
-                addChar(c);
-            }
-            
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+    public Lexer(String filename){
+        try{
+            this.pushbackReader = new PushbackReader(new BufferedReader(new FileReader(filename)), 1);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("Error getting file : " + e.getMessage());
         }
     }
 
-    private void addChar(char c){
-        currentToken += c;
-        System.out.println(currentToken);
+    public LexerToken getToken(){
+        try{
+            int ValueChar;
+            while ((ValueChar = this.pushbackReader.read()) != -1) {
+                char c = (char) ValueChar;
 
-        LexerToken token = LexerToken.fromText(currentToken);
-        if(token != null){
-            System.out.println(token);
+                LexerToken temp = addChar(c);
+                if(temp != null){
+                    return temp;
+                }
+            }
+            return LexerToken.fromText(currentToken);
+        } 
+        catch (IOException e) {
+            System.err.println("Error reading file : " + e.getMessage());
+            return null;
         }
+    }
+
+    private LexerToken addChar(char c){
+        currentToken += c;
+
+        return LexerToken.fromText(currentToken);
     }
 }
